20a593ab8d5b25719f7e22c6abffe4c85b379c0a
nehaprajapati123 <91823695+nehaprajapati123@users.noreply.github.com>
2021-11-22 21:15:14 +0530

    Update first.py

--- first.py
+++ first.py
@@ -15,38 +15,14 @@
 # import json
 # a={"neha":4}
 # x=json.dumps(a)
-# print(x)...........................dumps
+# print(x)
 
 # print(type(x))
-
-# import json
-# a={"neha":4}
-# x=open("my_file.json","w")
-# f=x.write("my name is neha")
-# x.close()
-# print(f)
-
-# import json
-# a={"neha":4}
-# x=open("my_file.json","r")
-# f=x.read()
-# x.close()
-# print(f)
-
-# import json
-# a={"neha":9}
-# x=open("my_file.json","r")
-# f=x.read()
-
-# json.dumps(f)
-# print(f)
-# x.close()
-
 # import json
 # a={"kc":9}
 # x=open("my_file.json","w")
 # json.dump(a,x,indent=3)
-# x.close()...............................dump
+# x.close() 
 
 
 
