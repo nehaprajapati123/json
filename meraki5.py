0f922167a8798d971f52f2e0ff9b9af9fcceb883
nehaprajapati123 <91823695+nehaprajapati123@users.noreply.github.com>
2021-11-22 19:31:52 +0530

    Update meraki5.py

--- meraki5.py
+++ meraki5.py
@@ -1,14 +1,25 @@
 # Q5.Json string ko check karo ki wo complex object contain karti hai ya nahi?
-dic='{"nam@e:"neh1a,"khf:"jhyf","ghf":"juyf"}'
-for i in dic:
-    for j in i:
-        if j<="a" and j>="z":
-            pass
-        else:
-            print(j,"complex is contain")
-    for k in dic[i]:
-        if k<="a" and k>="z":
-            pass
-        else:
-            print(k,"complex is contain")
 
+import json
+# Json string ko check karo ki bo complex object contain karti hai ya nahi?
+
+dic='{"3+j":"4-i"}'
+x=json.loads(dic)
+for i in x:           
+    for j in i:
+        if j>=chr(97) and j<=chr(122):
+            pass
+        elif j>=chr(65) and j<=chr(90):
+            pass
+        
+        else:
+            print(i,"json string comtain complex object")
+for k in x[i]:
+    for l in k:
+        if l>=chr(97) and l<=chr(122):
+            pass
+        elif l>=chr(65) and l<=chr(90):
+            pass
+        
+        else:
+            print(x[i],"json string comtain complex object")
