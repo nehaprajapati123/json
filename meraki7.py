bb7727b0a4b0210efe76e7edb387593bb75c2553
nehaprajapati123 <91823695+nehaprajapati123@users.noreply.github.com>
2021-11-22 19:34:16 +0530

    Update meraki7.py

--- meraki7.py
+++ meraki7.py
@@ -15,5 +15,20 @@
 #     “Age”: “29”
 #     }
 
+dic={
+    "Name":"Abhishek",
+    "Designation": "CEO of Navgurukul",
+    "genter":"male",
+    "age": "29"
+}
+import json
+x=json.dumps(dic)
+print(x)
 
+import json
+for i in dic:
+    y=open("files.txt","a")
+    json.dump(i,y)
+    json.dump(dic[i],y,)
+y.close()
 
