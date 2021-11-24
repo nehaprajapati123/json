a4e7ea67c623238623c3268117eda452d90585db
nehaprajapati123 <91823695+nehaprajapati123@users.noreply.github.com>
2021-11-24 19:05:04 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,39 @@
+# import json
+# dic={"name":"neha"}
+# x=json.dumps(dic)
+# print(x)
+# print(type(x))
+
+# import json
+# dic1={"name":"neha"}
+# dic2=open("file.text","w")
+# json.dump(dic1,dic2)
+# dic2.close()
+
+# dic='{"name":"neha"}'
+# import json
+# x=json.loads(dic)
+# print(x)
+# print(type(x))
+
+# import json
+# dic={"name":"neha"}
+# x=json.dumps(dic)
+# print(x)
+
+# import json
+# dic={"name":"neha"}
+# f=open("n.json","w")
+# v=json.dump(dic,f)
+# f.close
+
+# import json
+# dic='{"name":"neha"}'
+# v=json.loads(dic)
+# print(v)
+
+# import json
+# f=open("n.json")
+# v=json.load(f)
+# print(v)
+# f.close()
